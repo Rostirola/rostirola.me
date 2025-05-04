de607cafea6a653fb6a1777d246fca6e14bed9d5
Juan Rostirola <juanvitorrostirola666@gmail.com>
2025-05-04 00:31:05 -0300

    fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
+import Sobre from "@/app/sobre/page";
+
 export default function Home() {
     return (
-        <div style={{ height: "100vh" }}>
-            <h1>Home</h1>
-        </div>
+        <Sobre/>
     )
 }
